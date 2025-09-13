6845a35743dc1f07648ed3857c0c7769a6a82f5f
FrancoQuiroga01 <fquiroga@frba.utn.edu.ar>
2025-09-12 23:13:23 -0300

    cambios para conexion  con  app2

--- src/main/java/ar/edu/utn/dds/k3003/model/config/FuentesConfig.java
+++ src/main/java/ar/edu/utn/dds/k3003/model/config/FuentesConfig.java
@@ -1,21 +1,24 @@
 package ar.edu.utn.dds.k3003.model.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 @Profile("!stub")
 public class FuentesConfig {
 
     @Bean
-    public RestTemplate restTemplate(){
-        var factory  = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(30000);
-        return new RestTemplate(factory);
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.setConnectTimeout(Duration.ofSeconds(3))
+                .setReadTimeout(Duration.ofSeconds(3))
+                .build();
     }
 
     public String fuentesBaseUrl(@Value("${fuentes.base-url}") String baseUrl){
